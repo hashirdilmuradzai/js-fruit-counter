d1aebf2bbdb8a46421738beb03c1243ec4dabbec
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-07-14 16:49:54 +0500

    Update Code

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let fruits = ["mango", "apple", "watermelon", "apple", "grape", "Apple"];
+// Yha sy koe apna fruit name likhyga propmt ky through
+let fruitName = prompt("Enter fruit name to check available in stock");
+// Yha pr fruit stocks hai array ky andr
+let fruitStocks = ["mango", "apple", "watermelon", "apple", "grape", "Apple"];
+// ye fruit stock ka variable hai yha pr fruit stock ka data aaega kitny hai abhi available stock me
 let fruitCount = 0;
 
-for (let i = 0; i <= 6; i++) {}
+// loop ky zariyeee hum hum find karengy stock fruit k
+for (let i = 0; i < fruitStocks.length; i++) {
+  // condition lagaengy agr same name k miljae to stock me dekh kr btady kitny hai and alphabatic case lower kr rahy hai taky issue na ho
+  if (fruitName.toLowerCase() === fruitStocks[i].toLowerCase()) {
+    fruitCount++;
+  }
+}
+
+console.log(fruitCount);
